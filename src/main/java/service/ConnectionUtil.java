9e0f3edda56449499daa3ea498eab3112050506c
Vera Llugiqi <vera.llugiqi@outlook.com>
2023-05-16 23:24:11 +0200

    Update ConnectionUtil.java

--- src/main/java/service/ConnectionUtil.java
+++ src/main/java/service/ConnectionUtil.java
@@ -5,15 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
+
     private static Connection connection;
 
-    public static Connection getConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            String url = "jdbc:mysql://localhost:3306/knk2023";
-            String user = "root";
-            String password = "root";
-            connection = DriverManager.getConnection(url, user, password);
+    public static Connection getConnection() {
+        String databaseName = "knk_orari";
+        String databaseUser = "root";
+        String databasePassword = "";
+        String url = "jdbc:mysql://localhost/" + databaseName;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, databaseUser, databasePassword);
+        } catch(Exception e) {
+            e.printStackTrace();
+            e.getCause();
         }
+
         return connection;
     }
 }
