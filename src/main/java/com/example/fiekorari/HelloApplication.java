69285a1285c44f954c99f6dd10822a0c52f1717c
valtrinacacaj <valtrina.cacaj@student.uni-pr.edu>
2023-05-16 18:38:45 +0200

    updates

--- src/main/java/com/example/fiekorari/HelloApplication.java
+++ src/main/java/com/example/fiekorari/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fillimi.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 900);
         stage.setTitle("Zana<3!");
         stage.setScene(scene);
         stage.show();
@@ -19,6 +19,6 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-        System.out.println("Zana ma e mira");
+
     }
 }
