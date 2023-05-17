2dde02439295232fba28600331889fe708bda66c
valtrinacacaj <valtrina.cacaj@student.uni-pr.edu>
2023-05-17 11:57:32 +0200

    COLORS

--- src/main/java/com/example/fiekorari/HelloApplication.java
+++ src/main/java/com/example/fiekorari/HelloApplication.java
@@ -7,27 +7,29 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-<<<<<<< Updated upstream
+//<<<<<<< Updated upstream
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fillimi.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 400);
         stage.setTitle("Fillimi");
-=======
-        public class HelloApplication extends Application {
+//=======
+        class helloApplication extends Application {
             @Override
             public void start(Stage stage) throws IOException {
                 FXMLLoader fxmlLoader = new FXMLLoader(com.example.fiekorari.HelloApplication.class.getResource("fillimi.fxml"));
                 Scene scene = new Scene(fxmlLoader.load(), 900, 400);
-        stage.setTitle("FIEK - ORARI");
->>>>>>> Stashed changes
-        stage.setScene(scene);
-        stage.show();
-    }
+                stage.setTitle("FIEK - ORARI");
+//>>>>>>> Stashed changes
+                stage.setScene(scene);
+                stage.show();
+            }
 
-    public static void main(String[] args) {
-        launch();
+            public static void main(String[] args) {
+                launch();
 
+            }
+        }
     }
 }
