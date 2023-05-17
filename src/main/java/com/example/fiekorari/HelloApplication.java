7c96950f77eeea4d3ee45eda479e88cb4383aac5
valtrinacacaj <valtrina.cacaj@student.uni-pr.edu>
2023-05-17 11:32:57 +0200

    updates

--- src/main/java/com/example/fiekorari/HelloApplication.java
+++ src/main/java/com/example/fiekorari/HelloApplication.java
@@ -7,12 +7,21 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+<<<<<<< Updated upstream
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fillimi.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 400);
         stage.setTitle("Fillimi");
+=======
+        public class HelloApplication extends Application {
+            @Override
+            public void start(Stage stage) throws IOException {
+                FXMLLoader fxmlLoader = new FXMLLoader(com.example.fiekorari.HelloApplication.class.getResource("fillimi.fxml"));
+                Scene scene = new Scene(fxmlLoader.load(), 900, 400);
+        stage.setTitle("FIEK - ORARI");
+>>>>>>> Stashed changes
         stage.setScene(scene);
         stage.show();
     }
