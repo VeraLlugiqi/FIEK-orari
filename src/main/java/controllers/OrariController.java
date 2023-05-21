bd07687fea21b958bac7a960589bc45a8401d353
ZanaMisini <118012775+ZanaMisini@users.noreply.github.com>
2023-05-21 22:51:31 +0200

    Update OrariController.java

--- src/main/java/controllers/OrariController.java
+++ src/main/java/controllers/OrariController.java
@@ -24,10 +24,78 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OrariController {
-    String day;
+    String sid;
     String timestamp;
-    String salla;
-    String lenda;
-    String prof;
-    
+    String day;
+
+    public OrariController(String sid, String timestamp, String day) {
+        this.sid = sid;
+        this.timestamp = timestamp;
+        this.day = day;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+    @FXML
+    private BarChart<String, Integer> barChart;
+
+    @FXML
+    private CategoryAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
+
+    public void initialize() {
+        // Connect to the database
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/knk_orari", "root", "valtrina1*");
+             Statement statement = connection.createStatement()) {
+
+            // Execute the query
+            ResultSet resultSet = statement.executeQuery("SELECT day, COUNT(*) AS booked_schedules FROM orarizgjedhur GROUP BY day");
+
+            // Create a list to hold the XYChart.Series objects
+            List<XYChart.Series<String, Integer>> seriesList = new ArrayList<>();
+
+            // Create a series for the chart
+            XYChart.Series<String, Integer> series = new XYChart.Series<>();
+
+            // Iterate over the result set and add data to the series
+            while (resultSet.next()) {
+                String day = resultSet.getString("day");
+                int count = resultSet.getInt("booked_schedules");
+                series.getData().add(new XYChart.Data<>(day, count));
+            }
+
+            // Add the series to the list
+            seriesList.add(series);
+
+            // Set the data to the bar chart
+            barChart.getData().addAll(seriesList);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
