4a14178d5bb516c067b7de2c53d4e27b9bd80607
Vera Llugiqi <vera.llugiqi@outlook.com>
2023-05-21 23:58:00 +0200

    language profile

--- src/main/java/controllers/PasswordUpdateController.java
+++ src/main/java/controllers/PasswordUpdateController.java
@@ -3,20 +3,24 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import service.ConnectionUtil;
 import service.PasswordUtil;
+import service.Translate;
 
 import java.io.IOException;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class PasswordUpdateController extends SceneController {
     ActionEvent actionEvent;
@@ -29,7 +33,40 @@ public class PasswordUpdateController extends SceneController {
     @FXML
     private PasswordField confirmNewPasswordField;
     @FXML
+    Label fiek_orariLabel;
+    @FXML
+    Button startButton;
+    @FXML
+    Button manageClassButton;
+    @FXML
+    Button profileButton;
+    @FXML
+    Button Oraributton;
+    @FXML
+    Button NdihmaButton;
+    @FXML
+    Button logoutButton;
+    @FXML
     private Button saveChangesButton;
+    @FXML
+    Label profileTextLabel;
+    @FXML
+    Label profileLabel;
+    @FXML
+    Label repeatPasswordLabel;
+    @FXML
+    Label newPasswordLabel;
+    @FXML
+    Label passwordLabel;
+    @FXML
+    Label idNumberLabel;
+
+    public static String selectedLanguageCode = "sq";
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        updateTexts(); // Call updateTexts() during initialization
+    }
 
 
     public void updatePassword(ActionEvent event) {
@@ -146,6 +183,33 @@ public class PasswordUpdateController extends SceneController {
     public void switchToClose(ActionEvent event) {
         System.exit(0);
     }
+
+    public void updateTexts() {
+        logoutButton.setText(Translate.get("logoutButton.text"));
+        NdihmaButton.setText(Translate.get("NdihmaButton.text"));
+        Oraributton.setText(Translate.get("Oraributton.text"));
+        profileButton.setText(Translate.get("profileButton.text"));
+        manageClassButton.setText(Translate.get("manageClassButton.text"));
+        startButton.setText(Translate.get("startButton.text"));
+        fiek_orariLabel.setText(Translate.get("fiek_orariLabel.text"));
+        profileTextLabel.setText(getTranslation("profileTextLabel.text"));
+        profileLabel.setText(getTranslation("profileLabel.text"));
+        saveChangesButton.setText(getTranslation("saveChangesButton.text"));
+        repeatPasswordLabel.setText(getTranslation("repeatPasswordLabel.text"));
+        newPasswordLabel.setText(getTranslation("newPasswordLabel.text"));
+        passwordLabel.setText(getTranslation("passwordLabel.text"));
+        idNumberLabel.setText(getTranslation("idNumberLabel.text"));
+
+    }
+    private String getTranslation(String key) {
+        return Translate.get(key, selectedLanguageCode);
+    }
+
+    public void setSelectedLanguageCode(String languageCode) {
+        selectedLanguageCode = languageCode;
+    }
+
+
 }
 
 
