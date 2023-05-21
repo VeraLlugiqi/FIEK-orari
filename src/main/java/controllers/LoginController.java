988c380ebc6da4b9452a513b49728fe52c7a0b67
Vera Llugiqi <vera.llugiqi@outlook.com>
2023-05-21 02:03:57 +0200

    fixed

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,120 +1,4 @@
-//<<<<<<< HEAD
-//package controllers;
-//
-//
-//import java.io.IOException;
-//import java.security.MessageDigest;
-//import java.security.NoSuchAlgorithmException;
-//import java.security.SecureRandom;
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.Base64;
-//import javafx.scene.Parent;
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Node;
-//import javafx.scene.Scene;
-//import javafx.scene.control.Alert;
-//import javafx.scene.control.PasswordField;
-//import javafx.scene.control.TextField;
-//import javafx.stage.Stage;
-//import service.ConnectionUtil;
-//
-//public class LoginController {
-//    @FXML
-//    private TextField idTextField;
-//    @FXML
-//    private PasswordField passwordField;
-//    private Stage stage;
-//    private Scene scene;
-//    private Parent root;
-//
-//
-//    public void loginUser() {
-//        String idNumber = idTextField.getText();
-//        String password = passwordField.getText();
-//
-//        // Validate if any field is empty
-//        if (idNumber.isEmpty() || password.isEmpty()) {
-//            showErrorAlert("ID Number and password are required.");
-//            return;
-//        }
-//
-//        // Retrieve the user from the database
-//        try (Connection conn = ConnectionUtil.getConnection();
-//             PreparedStatement statement = conn.prepareStatement("SELECT * FROM user WHERE idNumber = ?")) {
-//            statement.setString(1, idNumber);
-//            ResultSet resultSet = statement.executeQuery();
-//
-//            if (resultSet.next()) {
-//                String storedPassword = resultSet.getString("password");
-//                byte[] salt = hexStringToByteArray(resultSet.getString("salt"));
-//                String hashedPassword = hashPassword(password, salt);
-//
-//                if (storedPassword.equals(hashedPassword)) {
-//                    // Login successful, proceed to the next screen or action
-//                    showAlert("Login successful!");
-//                } else {
-//                    showErrorAlert("Invalid idNumber or password.");
-//                }
-//            } else {
-//                showErrorAlert("Invalid idNumber or password.");
-//            }
-//        } catch (SQLException e) {
-//            showErrorAlert("Failed to login. Please try again.");
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private String hashPassword(String password, byte[] salt) {
-//        try {
-//            MessageDigest md = MessageDigest.getInstance("SHA-256");
-//            md.update(salt);
-//            byte[] hashedPassword = md.digest(password.getBytes());
-//            return Base64.getEncoder().encodeToString(hashedPassword);
-//        } catch (NoSuchAlgorithmException e) {
-//            throw new RuntimeException("Failed to hash password.", e);
-//        }
-//    }
-//
-//    private void showAlert(String message) {
-//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-//        alert.setHeaderText(null);
-//        alert.setContentText(message);
-//        alert.showAndWait();
-//    }
-//
-//    private void showErrorAlert(String message) {
-//        Alert alert = new Alert(Alert.AlertType.ERROR);
-//        alert.setHeaderText(null);
-//        alert.setContentText(message);
-//        alert.showAndWait();
-//    }
-//
-//    private byte[] hexStringToByteArray(String hexString) {
-//        int len = hexString.length();
-//        byte[] data = new byte[len / 2];
-//        for (int i = 0; i < len; i += 2) {
-//            data[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4)
-//                    + Character.digit(hexString.charAt(i + 1), 16));
-//        }
-//        return data;
-//    }
-//
-//    public void switchToSignUp(ActionEvent event) throws IOException {
-//        root = FXMLLoader.load(getClass().getResource("signup.fxml"));
-//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setTitle("Regjistrohu");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//}
-//=======
+
 package controllers;
 
 
@@ -124,12 +8,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-<<<<<<< Updated upstream
-=======
 import java.util.Locale;
 import java.util.ResourceBundle;
 import service.Translate;
->>>>>>> Stashed changes
 
 import javafx.scene.Parent;
 import javafx.event.ActionEvent;
@@ -137,9 +18,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import models.LocaleBundle;
 import models.User;
@@ -148,13 +27,9 @@ import service.PasswordUtil;
 
 public class LoginController {
     private UserController userController;
-<<<<<<< Updated upstream
-    public LoginController(){
-=======
     private ResourceBundle resources;
 
     public LoginController() {
->>>>>>> Stashed changes
         userController = new UserController();
     }
 
@@ -162,12 +37,19 @@ public class LoginController {
     private TextField idTextField;
     @FXML
     private PasswordField passwordField;
+    @FXML
+    private Label fiek_orariLabel;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private Label notRegisteredLabel;
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-<<<<<<< Updated upstream
-=======
    public static String selectedLanguageCode = "sq"; // Default language code
 //
 //    public void initialize() {
@@ -184,7 +66,6 @@ public class LoginController {
         notRegisteredLabel.setText(Translate.get("notRegisteredLabel.text"));
         signUpButton.setText(Translate.get("signUpButton.text"));
     }
->>>>>>> Stashed changes
 
     public void loginUser() {
         String idNumber = idTextField.getText();
@@ -192,11 +73,7 @@ public class LoginController {
 
         // Validate if any field is empty
         if (idNumber.isEmpty() || password.isEmpty()) {
-<<<<<<< Updated upstream
-            showErrorAlert("ID Number and password are required.");
-=======
             showErrorAlert(Translate.get("login.error.emptyFields"));
->>>>>>> Stashed changes
             return;
         }
 
@@ -224,20 +101,6 @@ public class LoginController {
                         stage.setScene(scene);
                         stage.show();
                     } catch (IOException e) {
-<<<<<<< Updated upstream
-                        showErrorAlert("Failed to load the next screen. Please try again.");
-                        e.printStackTrace();
-                    }
-                } else {
-                    showErrorAlert("Invalid idNumber or password.");
-                }
-
-            } else {
-                showErrorAlert("Invalid idNumber or password.");
-            }
-        } catch (SQLException e) {
-            showErrorAlert("Failed to login. Please try again.");
-=======
                         showErrorAlert(Translate.get("login.error.loadScreen"));
                         e.printStackTrace();
                     }
@@ -250,7 +113,6 @@ public class LoginController {
             }
         } catch (SQLException e) {
             showErrorAlert(Translate.get("login.error.loginFailed"));
->>>>>>> Stashed changes
             e.printStackTrace();
         }
     }
@@ -269,17 +131,11 @@ public class LoginController {
         root = FXMLLoader.load(getClass().getResource("/com/example/fiekorari/signup.fxml"), bundle);
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
-<<<<<<< Updated upstream
-        stage.setTitle("Regjistrohu");
-=======
         stage.setTitle(Translate.get("signupButton.text"));
->>>>>>> Stashed changes
         stage.setScene(scene);
         stage.show();
     }
 
-<<<<<<< Updated upstream
-=======
     public void switchToEnglish(ActionEvent event) {
         Translate.setLocale(Locale.ENGLISH);
         updateTexts();
@@ -292,7 +148,6 @@ public class LoginController {
     }
 
 
->>>>>>> Stashed changes
     public static byte[] hexStringToByteArray(String hexString) {
         int len = hexString.length();
         byte[] byteArray = new byte[len / 2];
