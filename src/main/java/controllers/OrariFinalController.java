69e1fda4270227a0433ec517bbcf110b2ab56dde
ZanaMisini <118012775+ZanaMisini@users.noreply.github.com>
2023-05-21 23:10:03 +0200

    orari updated

--- src/main/java/controllers/OrariFinalController.java
+++ src/main/java/controllers/OrariFinalController.java
@@ -1,16 +1,30 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import service.ConnectionUtil;
 import service.Translate;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
-public class OrariFinalController extends SceneController {
+public class OrariFinalController extends SceneController implements Initializable {
+    Connection conn;
+    PreparedStatement ps;
+    ResultSet rs;
+    ObservableList lista;
     @FXML
     Label fiek_orariLabel;
     @FXML
@@ -31,10 +45,54 @@ public class OrariFinalController extends SceneController {
     Label scheduleLabel;
 
     public static String selectedLanguageCode = "sq";
-    String sid;
-    String timestamp;
-    String day;
+    @FXML
+    private TableView<?> orari_table;
+    @FXML
+    private TableColumn<?, ?> columnDita;
+
+    @FXML
+    private TableColumn<?, ?> columnOra;
+    @FXML
+    private TableColumn<?, ?> columnSalla;
+    @FXML
+    private TableColumn<?, ?> columnLenda;
+
     ActionEvent actionEvent;
+
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        try{
+            conn = ConnectionUtil.getConnection();
+            lista = FXCollections.observableArrayList();
+            setCellTable();
+            loadFromDatabase();
+        }catch(Exception e){
+
+        }
+        updateTexts();
+
+    }
+
+    public void setCellTable(){
+        columnDita.setCellValueFactory(new PropertyValueFactory<>("day"));
+        columnLenda.setCellValueFactory(new PropertyValueFactory<>("lenda"));
+        columnOra.setCellValueFactory(new PropertyValueFactory<>("timestamp"));
+        columnSalla.setCellValueFactory(new PropertyValueFactory<>("salla"));
+    }
+
+    public void loadFromDatabase(){
+        try{
+            System.out.println(UserController.loggedInUserId);
+            ps = conn.prepareStatement("Select * from orarizgjedhur where availableOrariZgjedhur!=0");
+            rs = ps.executeQuery();
+            while(rs.next()){
+                lista.add(new OrariTable(rs.getString(7), rs.getString(6), rs.getString(4), rs.getString(5)));
+            }
+            orari_table.setItems(lista);
+
+        }catch(Exception e){
+
+        }
+    }
 
     public void switchToFillimi() throws IOException{
         switchToFillimi(actionEvent);
@@ -55,9 +113,6 @@ public class OrariFinalController extends SceneController {
         switchToOrari(actionEvent);
     }
 
-    public void initialize(URL url, ResourceBundle resourceBundle){
-        updateTexts();
-    }
     public void setSelectedLanguageCode(String languageCode) {
         selectedLanguageCode = languageCode;
     }
---
+++ src/main/java/controllers/OrariTable.java
@@ -0,0 +1,48 @@
+package controllers;
+
+public class OrariTable {
+    String day;
+    String timestamp;
+    String salla;
+    String lenda;
+
+    public OrariTable(String day, String timestamp, String salla, String lenda) {
+        this.day = day;
+        this.timestamp = timestamp;
+        this.salla = salla;
+        this.lenda = lenda;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSalla() {
+        return salla;
+    }
+
+    public void setSalla(String salla) {
+        this.salla = salla;
+    }
+
+    public String getLenda() {
+        return lenda;
+    }
+
+    public void setLenda(String lenda) {
+        this.lenda = lenda;
+    }
+
+}
