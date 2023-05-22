08034733f96ba9da4c937b7ca0923a543386adca
Vera Llugiqi <vera.llugiqi@outlook.com>
2023-05-22 17:21:12 +0200

    change

--- src/main/java/com/example/fiekorari/HelloApplication.java
+++ src/main/java/com/example/fiekorari/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application  {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("logIn.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menaxhoOret.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle(Translate.get("login.text"));
         stage.setScene(scene);
